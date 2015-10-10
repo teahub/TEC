139883281796dca0a616d0595e229470556b9233
Vinícius Rodrigues <viniciusclerod@gmail.com>
2015-10-10 16:37:39 -0300

    Update title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TEC' });
+  res.render('index', { title: 'TEC 2015' });
 });
 
 module.exports = router;
